df63802ad0520b9e6f3c0d48e5335a52e8f470b5
CodeGumi117 <xxgumimodzxx@gmail.com>
2024-12-24 23:07:06 +0100

    Update debugger.lua

--- debugger.lua
+++ debugger.lua
@@ -1,1 +1,12 @@
-print(1 + 1)
+local function Addition(num1, num2)
+  return (num1 + num2)
+end
+local function Subtraction(num1, num2)
+  return (num1 - num2)
+end
+local function Division(num1, num2)
+  return (num1 / num2)
+end
+local function Multiplication(num1, num2)
+  return (num1 * num2)
+end
